0bf7408346cb25ce496cc0e3ce5a1fae086bb460
akayo06 <pty106@gmail.com>
2023-05-08 19:40:08 +0800

    delete notnullable

--- express/migrations/20230505053324_auto-migrate.ts
+++ express/migrations/20230505053324_auto-migrate.ts
@@ -4,10 +4,10 @@ export async function up(knex: Knex): Promise<void> {
   if (!(await knex.schema.hasTable("user"))) {
     await knex.schema.createTable("user", (table) => {
       table.increments("id");
-      table.string("username", 32).notNullable();
-      table.string("email", 60).notNullable();
-      table.string("password", 30).notNullable();
-      table.boolean("is_admin").notNullable();
+      table.string("username", 32);
+      table.string("email", 60);
+      table.string("password", 30);
+      table.boolean("is_admin");
       table.timestamps(false, true);
     });
   }
@@ -15,16 +15,16 @@ export async function up(knex: Knex): Promise<void> {
   if (!(await knex.schema.hasTable("food"))) {
     await knex.schema.createTable("food", (table) => {
       table.increments("id");
-      table.string("name", 255).notNullable();
-      table.string("type", 255).notNullable();
-      table.decimal("energy").notNullable();
-      table.decimal("protein").notNullable();
-      table.decimal("total_fat").notNullable();
-      table.decimal("saturated_fat").notNullable();
-      table.decimal("trans_fat").notNullable();
-      table.decimal("carbonhydrate").notNullable();
-      table.decimal("sugars").notNullable();
-      table.decimal("sodium").notNullable();
+      table.string("name", 255);
+      table.string("type", 255);
+      table.decimal("energy");
+      table.decimal("protein");
+      table.decimal("total_fat");
+      table.decimal("saturated_fat");
+      table.decimal("trans_fat");
+      table.decimal("carbonhydrate");
+      table.decimal("sugars");
+      table.decimal("sodium");
       table.timestamps(false, true);
     });
   }
@@ -32,10 +32,10 @@ export async function up(knex: Knex): Promise<void> {
   if (!(await knex.schema.hasTable("diet_record"))) {
     await knex.schema.createTable("diet_record", (table) => {
       table.increments("id");
-      table.datetime("date").notNullable();
-      table.integer("user_id").unsigned().notNullable().references("user.id");
-      table.enum("section", ["breakfast", "lunch", "dinner"]).notNullable();
-      table.boolean("active").notNullable();
+      table.datetime("date");
+      table.integer("user_id").unsigned().references("user.id");
+      table.enum("section", ["breakfast", "lunch", "dinner"]);
+      table.boolean("active");
       table.timestamps(false, true);
     });
   }
@@ -46,9 +46,9 @@ export async function up(knex: Knex): Promise<void> {
       table
         .integer("diet_record_id")
         .unsigned()
-        .notNullable()
+
         .references("diet_record.id");
-      table.integer("food_id").unsigned().notNullable().references("food.id");
+      table.integer("food_id").unsigned().references("food.id");
       table.timestamps(false, true);
     });
   }
@@ -56,10 +56,10 @@ export async function up(knex: Knex): Promise<void> {
   if (!(await knex.schema.hasTable("shape_record"))) {
     await knex.schema.createTable("shape_record", (table) => {
       table.increments("id");
-      table.integer("user_id").unsigned().notNullable().references("user.id");
-      table.datetime("date").notNullable();
-      table.decimal("height").notNullable();
-      table.decimal("weight").notNullable();
+      table.integer("user_id").unsigned().references("user.id");
+      table.datetime("date");
+      table.decimal("height");
+      table.decimal("weight");
       table.timestamps(false, true);
     });
   }
@@ -67,15 +67,15 @@ export async function up(knex: Knex): Promise<void> {
   if (!(await knex.schema.hasTable("daily_nutrition_target"))) {
     await knex.schema.createTable("daily_nutrition_target", (table) => {
       table.increments("id");
-      table.integer("user_id").unsigned().notNullable().references("user.id");
-      table.decimal("energy").notNullable();
-      table.decimal("protein").notNullable();
-      table.decimal("total_fat").notNullable();
-      table.decimal("saturated_fat").notNullable();
-      table.decimal("trans_fat").notNullable();
-      table.decimal("carbonhydrate").notNullable();
-      table.decimal("sugars").notNullable();
-      table.decimal("sodium").notNullable();
+      table.integer("user_id").unsigned().references("user.id");
+      table.decimal("energy");
+      table.decimal("protein");
+      table.decimal("total_fat");
+      table.decimal("saturated_fat");
+      table.decimal("trans_fat");
+      table.decimal("carbonhydrate");
+      table.decimal("sugars");
+      table.decimal("sodium");
       table.timestamps(false, true);
     });
   }
@@ -83,8 +83,8 @@ export async function up(knex: Knex): Promise<void> {
   if (!(await knex.schema.hasTable("set"))) {
     await knex.schema.createTable("set", (table) => {
       table.increments("id");
-      table.integer("user_id").unsigned().notNullable().references("user.id");
-      table.string("set_name", 32).notNullable();
+      table.integer("user_id").unsigned().references("user.id");
+      table.string("set_name", 32);
       table.timestamps(false, true);
     });
   }
@@ -92,8 +92,8 @@ export async function up(knex: Knex): Promise<void> {
   if (!(await knex.schema.hasTable("mealset"))) {
     await knex.schema.createTable("mealset", (table) => {
       table.increments("id");
-      table.integer("food_id").unsigned().notNullable().references("food.id");
-      table.integer("set_id").unsigned().notNullable().references("set.id");
+      table.integer("food_id").unsigned().references("food.id");
+      table.integer("set_id").unsigned().references("set.id");
       table.timestamps(false, true);
     });
   }
