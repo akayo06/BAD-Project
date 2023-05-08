a460895bc7c9169c07014156a2980d2940ecaff5
akayo06 <pty106@gmail.com>
2023-05-08 17:13:56 +0800

    123

--- express/routes/scan.routes.ts
+++ express/routes/scan.routes.ts
@@ -1,7 +1,3 @@
 import express from "express";
 
-<<<<<<< HEAD
 export const scanRoute = express.Router();
-=======
-export const scanRoute = express.Router();
->>>>>>> 29e04f06f07f19576e835ed6239657a0136ecc85
